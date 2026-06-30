e47226ce1145e651e0d42f8ef0e87fca77111bf7
Tempestro <112181066+Tempestro@users.noreply.github.com>
2026-06-30 20:09:22 +0200

    Refactor tests and add a dedicated folder for the binaries produced

--- tests/tester.nim
+++ tests/tester.nim
@@ -1,4 +1,4 @@
-import std/os
+import std/[os, strutils]
 
 proc fatal(msg: string) = quit "FAILURE " & msg
 
@@ -6,12 +6,15 @@ proc exec(cmd: string) =
   if execShellCmd(cmd) != 0: fatal cmd
 
 
-template execBackend(cmd: string) =
-  exec "nim c " & cmd & " examples/hello.nim"
-  exec "nim c " & cmd & " examples/layout_demo.nim"
-  exec "nim c " & cmd & " examples/paint.nim"
-  exec "nim c " & cmd & " examples/todo.nim"
-
+template execBackend(backend: string) =
+  for example in walkFiles("examples/*.nim"):
+    let command: string = [
+    "nim c",
+    "--outdir:testArtifacts", # give the bins their own folder so they don't pollute the src folder
+    backend,
+    example
+    ].join(" ")
+    exec command
 
 execBackend("")
 when defined(linux):
